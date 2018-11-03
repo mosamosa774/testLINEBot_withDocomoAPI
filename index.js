aebb45710db906261e875476800fad06f2718267
mosamosa774 <44542163+mosamosa774@users.noreply.github.com>
2018-11-03 18:41:01 +0900

    Update index.js

--- index.js
+++ index.js
@@ -7,19 +7,19 @@ const line_config = {
     channelSecret: process.env.LINE_CHANNEL_SECRET // 環境変数からChannel Secretをセットしています
     };
 const bot = new line.Client(line_config);
+var request = require('sync-request');
 
-
-/*function getJSON(message) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "https://api.apigw.smt.docomo.ne.jp/naturalChatting/v1/dialogue?APIKEY="+apiKey, false);
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.onreadystatechange = function() {//状態が変化すると関数が呼び出されます。
-    if(this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-        return xhr.responseText;
-    }
-    xhr.send("{\"language\": \"ja-JP\",\"botId\": \"Chatting\",\"appId\": \""+appid+"\",\"voiceText\": \""+message+"\",\"clientData\": {\"option\": {\"nickname\": \"光\",\"nicknameY\": \"ヒカリ\",\"sex\": \"女\",\"bloodtype\": \"B\",\"birthdateY\": \"1997\",\"birthdateM\": \"5\",\"birthdateD\": \"30\",\"age\": \"16\",\"constellations\": \"双子座\",\"place\": \"東京\",\"mode\": \"dialog\"}},\"appRecvTime\": \"2015-05-05 13:30:00\",\"appSendTime\": \"2015-05-05 13:31:00\"}");
+function getJSON(message) {
+    var response = request(
+        'POST',
+        "https://api.apigw.smt.docomo.ne.jp/naturalChatting/v1/dialogue?APIKEY="+apiKey,
+        "Content-Type: application/json",
+        "{\"language\": \"ja-JP\",\"botId\": \"Chatting\",\"appId\": \""+appid+"\",\"voiceText\": \""+message+"\",\"clientData\": {\"option\": {\"nickname\": \"光\",\"nicknameY\": \"ヒカリ\",\"sex\": \"女\",\"bloodtype\": \"B\",\"birthdateY\": \"1997\",\"birthdateM\": \"5\",\"birthdateD\": \"30\",\"age\": \"16\",\"constellations\": \"双子座\",\"place\": \"東京\",\"mode\": \"dialog\"}},\"appRecvTime\": \"2015-05-05 13:30:00\",\"appSendTime\": \"2015-05-05 13:31:00\"}"
+    );
+    console.log("Status Code (function) : "+response.statusCode);
+    return response.json();
 }
-*/
+
 server.post('/webhook', line.middleware(line_config), (req, res, next) => {
     // 先行してLINE側にステータスコード200でレスポンスする。
     res.sendStatus(200);
@@ -32,6 +32,8 @@ server.post('/webhook', line.middleware(line_config), (req, res, next) => {
         // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
         if (event.type == "message" && event.message.type == "text"){
             // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
+            res_JSON = getJSON(event.message.text);
+            console.log(res_JSON);
             if (event.message.text == "こんにちは"){
                 // replyMessage()で返信し、そのプロミスをevents_processedに追加。
                 events_processed.push(bot.replyMessage(event.replyToken, {
