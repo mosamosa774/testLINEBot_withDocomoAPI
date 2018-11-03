d4ae6da930d9ac740993b91cb977c0f0dd67a47c
mosamosa774 <44542163+mosamosa774@users.noreply.github.com>
2018-11-03 17:49:03 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const server = require("express")();
 const line = require("@line/bot-sdk"); // Messaging APIのSDKをインポート
-
+const appid = process.env.DocomoAPIAppID
+const apiKey = process.env.DocomoAPIKey
 const line_config = {
     channelAccessToken: process.env.LINE_ACCESS_TOKEN, // 環境変数からアクセストークンをセットしています
     channelSecret: process.env.LINE_CHANNEL_SECRET // 環境変数からChannel Secretをセットしています
@@ -20,14 +21,17 @@ const bot = new line.Client(line_config);
             req.body.events.forEach((event) => {
                 // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
                 if (event.type == "message" && event.message.type == "text"){
-                    // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
-                    if (event.message.text == "こんにちは"){
-                        // replyMessage()で返信し、そのプロミスをevents_processedに追加。
-                        events_processed.push(bot.replyMessage(event.replyToken, {
-                            type: "text",
-                            text: "こんにちは！"
-                        }));
-                    }
+                   str = getJSON(event.message.text);
+                   events_processed.push(bot.replyMessage(event.replyToken, {
+                   type: "text",
+                   text: str
+                }));
+                }
+                else {
+                events_processed.push(bot.replyMessage(event.replyToken, {
+                   type: "text",
+                   text: "ちょっとその形式はわかりませんね。"
+                }));
                 }
             });
       
@@ -38,3 +42,15 @@ const bot = new line.Client(line_config);
                 }
             );
         });
+
+function getJSON(message) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://api.apigw.smt.docomo.ne.jp/naturalChatting/v1/dialogue?APIKEY=apiKey", false);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function() {//状態が変化すると関数が呼び出されます。
+    if(this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+        return req.responseText;
+    }
+    xhr.send("{\"language\": \"ja-JP\",\"botId\": \"Chatting\",\"appId\": \""+appid+"\",\"voiceText\": \"こんにちは\",\"clientData\": {\"option\": {\"nickname\": \"光\",\"nicknameY\": \"ヒカリ\",\"sex\": \"女\",\"bloodtype\": \"B\",\"birthdateY\": \"1997\",\"birthdateM\": \"5\",\"birthdateD\": \"30\",\"age\": \"16\",\"constellations\": \"双子座\",\"place\": \"東京\",\"mode\": \"dialog\"}},\"appRecvTime\": \"2015-05-05 13:30:00\",\"appSendTime\": \"2015-05-05 13:31:00\"}");
+}
+}
