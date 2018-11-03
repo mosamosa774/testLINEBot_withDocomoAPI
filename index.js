5fa0d8782743a162df3e40d946e197e12a759f00
mosamosa774 <s1220052@gmail.com>
2018-11-03 16:53:00 +0900

    Improve

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const server = require("express")();
 const line = require("@line/bot-sdk"); // Messaging APIのSDKをインポート
-const bot = new line.Client(line_config);
 
 const line_config = {
     channelAccessToken: process.env.LINE_ACCESS_TOKEN, // 環境変数からアクセストークンをセットしています
     channelSecret: process.env.LINE_CHANNEL_SECRET // 環境変数からChannel Secretをセットしています
     };
+const bot = new line.Client(line_config);
     server.listen(process.env.PORT || 3000);
         // -----------------------------------------------------------------------------
         // ルーター設定
