13fbcecd1983bbb9ebac92ce2b52888ea87caf5c
mosamosa774 <s1220052@gmail.com>
2018-11-03 16:41:38 +0900

    Improve

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+#!/bin/sh   
+git add . && git commit -m 'Improve' && git push
--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 // -----------------------------------------------------------------------------
-// const server = require("express")();
-// const line = require("@line/bot-sdk"); // Messaging APIのSDKをインポート
-//
-// const line_config = {
-//
-//     channelAccessToken: process.env.LINE_ACCESS_TOKEN, // 環境変数からアクセストークンをセットしています
-//         channelSecret: process.env.LINE_CHANNEL_SECRET // 環境変数からChannel Secretをセットしています
-//         };
-//
-//         server.listen(process.env.PORT || 3000);
-//
-//         server.post('/webhook', line.middleware(line_config), (req, res, next) => {
-//
-//             res.sendStatus(200);
-//
-//                 console.log(req.body);
-//
-//                 });
+const server = require("express")();
+const line = require("@line/bot-sdk"); // Messaging APIのSDKをインポート
+
+const line_config = {
+    channelAccessToken: process.env.LINE_ACCESS_TOKEN, // 環境変数からアクセストークンをセットしています
+    channelSecret: process.env.LINE_CHANNEL_SECRET // 環境変数からChannel Secretをセットしています
+    };
+    server.listen(process.env.PORT || 3000);
+    server.post('/webhook', line.middleware(line_config), (req, res, next) => {
+        res.sendStatus(200);
+        console.log(req.body);
+        });
