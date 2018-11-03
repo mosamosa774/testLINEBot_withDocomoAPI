5bd37e7e397e5505adeb60ba530c3e70e215a005
mosamosa774 <44542163+mosamosa774@users.noreply.github.com>
2018-11-03 22:01:49 +0900

    Update index.js

--- index.js
+++ index.js
@@ -7,24 +7,14 @@ const line_config = {
     channelSecret: process.env.LINE_CHANNEL_SECRET // 環境変数からChannel Secretをセットしています
     };
 const bot = new line.Client(line_config);
-var request = require('sync-request');
+var request = require('request');
 
-function getJSON(message) {
-    var response = request(
-        'POST',
-        'https://api.apigw.smt.docomo.ne.jp/naturalChatting/v1/dialogue?APIKEY='+apiKey,
-        'Content-Type: application/json',
-        '{\"language\": \"ja-JP\",\"botId\": \"Chatting\",\"appId\": \""+appid+"\",\"voiceText\": \""+message+"\",\"clientData\": {\"option\": {\"nickname\": \"光\",\"nicknameY\": \"ヒカリ\",\"sex\": \"女\",\"bloodtype\": \"B\",\"birthdateY\": \"1997\",\"birthdateM\": \"5\",\"birthdateD\": \"30\",\"age\": \"16\",\"constellations\": \"双子座\",\"place\": \"東京\",\"mode\": \"dialog\"}},\"appRecvTime\": \"2015-05-05 13:30:00\",\"appSendTime\": \"2015-05-05 13:31:00\"}'
-    );
-    console.log("Status Code (function) : "+response.statusCode);
-    return response.json();
-}
+server.listen(process.env.PORT || 3000);
 
 server.post('/webhook', line.middleware(line_config), (req, res, next) => {
     // 先行してLINE側にステータスコード200でレスポンスする。
     res.sendStatus(200);
-                res_JSON = getJSON("test");
-            console.log(res_JSON);
+
     // すべてのイベント処理のプロミスを格納する配列。
     let events_processed = [];
 
@@ -32,22 +22,30 @@ server.post('/webhook', line.middleware(line_config), (req, res, next) => {
     req.body.events.forEach((event) => {
         // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
         if (event.type == "message" && event.message.type == "text"){
-            // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
-
-            if (event.message.text == "こんにちは"){
-                // replyMessage()で返信し、そのプロミスをevents_processedに追加。
+            console.log(apiKey);
+            console.log(appid);
+            var options = {
+                url: 'https://api.apigw.smt.docomo.ne.jp/naturalChatting/v1/dialogue?APIKEY='+apiKey,
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json;charset=UTF-8'
+                },
+                json: {"language": "ja-JP",
+                "botId": "Chatting",
+                "appId": appid,
+                "voiceText": event.message.text
+                }
+            }
+            var msg = "南無";
+            request(options, function (error, response, body) {
+                msg = body['systemText']['expression'];
                 events_processed.push(bot.replyMessage(event.replyToken, {
                     type: "text",
-                    text: "これはこれは"
+                    text: msg
                 }));
-            }
+            })
         }
     });
 
-    // すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
-    Promise.all(events_processed).then(
-        (response) => {
-            console.log(`${response.length} event(s) processed.`);
-        }
-    );
 });
+
