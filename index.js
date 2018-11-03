387ca33d5257a6031fbce3f530c6fe9af3919d2a
mosamosa774 <44542163+mosamosa774@users.noreply.github.com>
2018-11-03 18:27:45 +0900

    Update index.js

--- index.js
+++ index.js
@@ -20,40 +20,32 @@ const bot = new line.Client(line_config);
     xhr.send("{\"language\": \"ja-JP\",\"botId\": \"Chatting\",\"appId\": \""+appid+"\",\"voiceText\": \""+message+"\",\"clientData\": {\"option\": {\"nickname\": \"光\",\"nicknameY\": \"ヒカリ\",\"sex\": \"女\",\"bloodtype\": \"B\",\"birthdateY\": \"1997\",\"birthdateM\": \"5\",\"birthdateD\": \"30\",\"age\": \"16\",\"constellations\": \"双子座\",\"place\": \"東京\",\"mode\": \"dialog\"}},\"appRecvTime\": \"2015-05-05 13:30:00\",\"appSendTime\": \"2015-05-05 13:31:00\"}");
 }
 */
-    server.listen(process.env.PORT || 3000);
-        // -----------------------------------------------------------------------------
-        // ルーター設定
-        server.post('/webhook', line.middleware(line_config), (req, res, next) => {
-            // 先行してLINE側にステータスコード200でレスポンスする。
-            res.sendStatus(200);
-     
-            // すべてのイベント処理のプロミスを格納する配列。
-            let events_processed = [];
-      
-            // イベントオブジェクトを順次処理。
-            req.body.events.forEach((event) => {
-                // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
-                if (event.type == "message" && event.message.type == "text"){
-                   //str = getJSON(event.message.text);
-                   //console.log("touch:"+str);
-                   events_processed.push(bot.replyMessage(event.replyToken, {
-                   type: "text",
-                   text: "hai"
-                }));
-                }
-                else {
-                events_processed.push(bot.replyMessage(event.replyToken, {
-                   type: "text",
-                   text: "ちょっとその形式はわかりませんね。"
-                }));
-                }
-            });
-      
-            // すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
-            Promise.all(events_processed).then(
-                (response) => {
-                    console.log(`${response.length} event(s) processed.`);
-                }
-            );
-        });
+server.post('/webhook', line.middleware(line_config), (req, res, next) => {
+    // 先行してLINE側にステータスコード200でレスポンスする。
+    res.sendStatus(200);
 
+    // すべてのイベント処理のプロミスを格納する配列。
+    let events_processed = [];
+
+    // イベントオブジェクトを順次処理。
+    req.body.events.forEach((event) => {
+        // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
+        if (event.type == "message" && event.message.type == "text"){
+            // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
+            if (event.message.text == "こんにちは"){
+                // replyMessage()で返信し、そのプロミスをevents_processedに追加。
+                events_processed.push(bot.replyMessage(event.replyToken, {
+                    type: "text",
+                    text: "これはこれは"
+                }));
+            }
+        }
+    });
+
+    // すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
+    Promise.all(events_processed).then(
+        (response) => {
+            console.log(`${response.length} event(s) processed.`);
+        }
+    );
+});
